61e210675cb459d037e9efb4d2f4943814b99fac
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-02-17 14:59:59 +0100

    fix

--- src/middlewares/checkContent.ts
+++ src/middlewares/checkContent.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { notAcceptable } from '../utils/responses';
 
 export default () => (req: Request, res: Response, next: NextFunction) => {
-  if (req.method === 'OPTIONS' || req.is('Content-Type') === 'application/json') {
+  if (req.method === 'OPTIONS' || req.get('Content-Type') === 'application/json') {
     return next();
   }
 
